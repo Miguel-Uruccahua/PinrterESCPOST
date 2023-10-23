d07bf4a1a2e21438f138bfa5d50bf41d12ffe04c
miguelUruccahua <desarrollomiguel1997@gmail.com>
2023-10-23 18:53:08 -0500

    usbAction

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -32,6 +32,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //LLamar a printUsb() para iniciar la impresión
+        //printUsb
     }
 
     private fun printUsb() {
